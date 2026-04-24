e8f47443c4af35eaa8a069c73441c9bdbc359976
Tuomas Huhtala <ag2929@student.jamk.fi>
2026-04-24 10:32:47 +0300

    Alku Button lisätty

---
+++ testiluApp/src/routes/CategoryButton.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	let { name, onclick }: { name: string; onclick?: () => void } = $props();
+</script>
+
+<button class="h-20 w-80 border-2 bg-slate-700" {onclick}>
+	{name}
+</button>
