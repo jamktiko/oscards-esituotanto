270d694ced4d946b8e9d740cd77a14ae60ba7cea
Tuomas Huhtala <ag2929@student.jamk.fi>
2026-04-24 11:03:14 +0300

    Lisäsin Kortti.svelte ja muokkasin new/+page.svelte

---
+++ testiluApp/src/lib/Kortti.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	let { cardTitle }: { cardTitle: string } = $props();
+</script>
+
+<div>
+	<button>
+		<div>
+			{cardTitle}
+		</div>
+	</button>
+</div>
---
+++ testiluApp/src/routes/new/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import Kortti from '$lib/Kortti.svelte';
+</script>
+
+<h1>Best ...</h1>
+<Kortti cardTitle="Tässä on kortti" />
+<Kortti cardTitle="Tässä on toinen kortti" />
